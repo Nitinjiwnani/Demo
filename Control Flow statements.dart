1c326ac2256a5d02867417258fc3b11afe2b7985
Nitin Jiwnani <jiwnani01@gmail.com>
2022-05-21 01:27:06 +0530

    If else

---
+++ Control Flow statements.dart
@@ -0,0 +1,12 @@
+void main() {
+  // IF and ELSE statements
+
+  var salary = 15000;
+
+  if (salary > 20000) {
+    print("You got promotion, congratulations!");
+  }
+  else{
+    print("You need to work hard");
+  }
+}
