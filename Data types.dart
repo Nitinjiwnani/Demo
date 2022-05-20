84785fd87558222846071fbe5efc62a4af23c2c8
Nitin Jiwnani <jiwnani01@gmail.com>
2022-05-21 00:35:23 +0530

    basic data types

---
+++ Data types.dart
@@ -0,0 +1,25 @@
+void main() {
+  //  Numbers: int
+  int score = 23;
+  var count = 23; //it is inferred as integer automatically by compiler
+  int hexValue = 0xEADEBAEE;
+
+  //numbers: double
+  double percentage = 93.4;
+  var percent = 82.222;
+  double exponents = 1.42e5;
+
+  //Strings
+  String name = "Henry";
+  var company = "Google";
+
+  //Boolean
+  bool isValid = true;
+  var isAlive = false;
+
+  print(score);
+  print(isValid);
+
+  //NOTE: all data types in dart are Objects
+  //Therefore, thier initial value is by default Null
+}
---
+++ playground.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) { //From where the program start
+//First line of code (Comment)
+  print("Hello World"); //Use print to print something
+  print("This is my first application");
+  print(4 - 1);
+  print(false);
+}
