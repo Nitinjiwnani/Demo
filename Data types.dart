fa0c6382648077587c1c6ffe0d0277a6e31c2398
Nitin Jiwnani <jiwnani01@gmail.com>
2022-05-21 01:21:35 +0530

    final and const

--- Data types.dart
+++ Data types.dart
@@ -1,30 +1,15 @@
 void main() {
-  //Literals
-  var isCool = true;
-  int x = 2;
-  "john";
-  4.5; //These are all literals and can  be assigned to some variables
+  //final keyword
+  final cityName = 'Mumbai';
 
-  //Various ways to define string literals in dart
-  String s1 = 'Single';
-  String s2 = "Double";
-  String s3 = 'It\'s easy'; //'It's easy';
-  String s4 = "It's easy";
+  final String countryName = 'India';
 
-  String s5 =
-      //'The code should be within  the margin and should not cross it, to overcome it we use plus';
-      'The code should be within  the margin and should not cross it,' //Using plus is bad practice so there's flexibility of not using it
-      'to overcome it we use plus';
+  //const keyword
+  const PI = 3.14;
+  const double gravity = 9.8;
+}
 
-  // String Interpolation
-  String name = "Kevin";
-
-  print("My name is $name"); // we use it to avoid print("My name is" + name);
-  print("The number of characters in string Kevin is ${name.length}");
-
-  int l = 20;
-  int b = 10;
-
-  print("The sum of $l and $b is ${l + b} ");
-  print("The area of rec with length $l and breadth $b is ${l * b}");
+class circle {
+  final color = 'Red';
+  static const PI = 3.14; //Cannot be declared const without static***imp
 }
